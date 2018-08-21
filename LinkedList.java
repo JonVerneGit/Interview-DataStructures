94d8a5fc7221a93ee1c79d5ca897f18880271cc4
JonVerneGit <42451032+JonVerneGit@users.noreply.github.com>
2018-08-21 15:39:05 -0700

    Class signiture and description comment

---
+++ LinkedList.java
@@ -0,0 +1,13 @@
+
+/**
+ * This class is designed to provide a functioning implementation 
+ * of the ListI interface. The LinkedList class provides all the 
+ * capabilities to construct and operate on a linked list data
+ * structure.
+ * 
+ * @author Jonathan Verne
+ *
+ * @param <E>
+ */
+
+public class LinkedList<E> implements ListI<E> {}
