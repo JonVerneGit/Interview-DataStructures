583f6bf4701e8960f56479e01cdc06c6f4bf0fef
JonVerneGit <42451032+JonVerneGit@users.noreply.github.com>
2018-08-27 11:26:45 -0700

    Create Hash.java
    Created class sig. and initial variables

---
+++ Hash.java
@@ -0,0 +1,24 @@
+package data_structures;
+
+import java.util.Iterator;
+
+/**
+ * The Hash data structure has O(1) time complexity (best case) for add, remove, and find
+ * for an object in the data structure. The methods in the Hash data structure are defined
+ * by the HashI interface. The Hash consists of an array of Linked Lists,
+ * the Linked Lists are defined by the HashListI interface.
+ * 
+ * @author Jonathan Verne
+ *
+ * @param <K> The key for entries in the hash
+ * @param <V> The value for entries in the hash
+ */
+
+public class Hash<K, V> implements HashI<K, V> {
+
+	int tableSize;
+	int numElements;
+	LinkedList<HashElement<K,V>> [] hash_array;
+	double maxLoadFactor;
+  
+  }
