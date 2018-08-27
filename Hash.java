4577f20c761b1c1362fc10127442cd922ca3d5dd
JonVerneGit <42451032+JonVerneGit@users.noreply.github.com>
2018-08-27 11:27:54 -0700

    Created HashElement class

--- Hash.java
+++ Hash.java
@@ -21,4 +21,34 @@ public class Hash<K, V> implements HashI<K, V> {
 	LinkedList<HashElement<K,V>> [] hash_array;
 	double maxLoadFactor;
   
+	/**
+	 * The Hash Element class provides methods to create a new Hash Element
+	 * to be added to the Hash Table. It also includes a CompareTo in order
+	 * to compare two hash elements.
+	 * @author Jonathan Verne
+	 *
+	 * @param <K> The key for the hash element
+	 * @param <V> The value for the hash element
+	 */
+	class HashElement<K, V> implements Comparable<HashElement<K,V>>{
+		K key;
+		V value;
+		
+		/**
+		 * Constructor for the Hash Element. Sets the key and value.
+		 * @param key
+		 * @param value
+		 */
+		public HashElement(K key,V value){
+			this.key = key;
+			this.value = value;
+		}
+		
+		/**
+		 * Compares two hash elements.
+		 */
+		public int compareTo(HashElement<K,V> o){
+			return (((Comparable<K>)o.key).compareTo(this.key));
+		}
+	}
   }
